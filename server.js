18ef756605ee66eea776d00e3652b6e6193122b8
chandini-190404 <davadulachandini@gmail.com>
2025-03-25 19:36:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,4 +40,4 @@ app.get("/checkout", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
